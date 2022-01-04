065cb6c08ec179d1ac1c66ee8113b25089b4b25a
tahaltuntas <93127440+tahaltuntas@users.noreply.github.com>
2022-01-05 02:00:25 +0300

    add uı compenents

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Player player;
+
+    public Text scoreText;
+    
+    public GameObject playButton;
+
+    public GameObject gameOver;
+
+    
     private int score;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;   //hedef kare hýzýmýzý ayarlýyoruz. 
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;  //0: oyunu duraklatýyor
+        player.enabled = false;
+    }
 
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 }
